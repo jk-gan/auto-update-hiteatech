fc64811b417befbd392b84e032bab9dabf56af5e
Jun Kai <kuhn96@gmail.com>
2016-04-04 20:18:01 +0800

    Modified commit msg

--- auto.rb
+++ auto.rb
@@ -15,10 +15,16 @@ ws = session.spreadsheet_by_key(ENV['KEY']).worksheets[0]
 
 page = Nokogiri::HTML(open("../hiteatech.github.io/git.html"))
 
+# count the number
+num_attendee = 0
+
 # Create the first li element if there is no li element
 unless page.at_css('.collection-item')
   ul = page.at_css('.collection')
   ul.children = %Q[<li class="collection-item" id="auto-collection-item">#{ws[2, 2]}<span class="badge">#{ws[2, 5]}</span></li>]
+
+  # update attendee count
+  num_attendee += 1
 end
 
 li = page.css('.collection-item')
@@ -32,20 +38,23 @@ unless max == ws.num_rows
     # refresh li
     li = page.css('.collection-item')
     max += 1
+
+    # update attendee count
+    num_attendee += 1
   end
 end
 
 # For Checking Purpose
-puts page.to_html
+# puts page.to_html
 
 # Overwrite to the file
 File.write("../hiteatech.github.io/git.html", page.to_html)
 
 # Run command to push to github
-puts %x{ cd #{target_path} && git cm -am "New user" && git push origin master }
+puts %x{ cd #{target_path} && git cm -am "Added #{num_attendee} attendee(s)" && git push origin master }
 
 # Yet another way to do so.
-p ws.rows  #==> [["fuga", ""], ["foo", "bar"]]
+# p ws.rows  #==> [["fuga", ""], ["foo", "bar"]]
 
 # Reloads the worksheet to get changes by other clients.
 ws.reload
